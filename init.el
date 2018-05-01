0bdc2029382f6f5d94700cdc45ed7f5569b8b03d
Dmitri Akatov <akatov@gmail.com>
2018-05-01 13:05:52 +0100

    new package config

--- init.el
+++ init.el
@@ -1,5 +1,7 @@
 (setq debug-on-error t)
-(package-initialize)
+(require 'cask "~/.cask/cask.el")
+(cask-initialize)
+;; (package-initialize)
 (org-babel-load-file
  (expand-file-name "readme.org"
 		   user-emacs-directory))
