a2d4c0554cb6b92e3114a1a671f8f44ed52dcaf1
Dmitri Akatov <akatov@gmail.com>
2019-07-09 16:53:46 +0100

    changing config

--- init.el
+++ init.el
@@ -1,7 +1,12 @@
-(setq debug-on-error t)
+'(setq debug-on-error t)
+
+;;; use Cask to configure the installed packages
 (require 'cask "~/.cask/cask.el")
 (cask-initialize)
+
+;;; TODO: why was this here?
 ;; (package-initialize)
+
 (org-babel-load-file
  (expand-file-name "readme.org"
 		   user-emacs-directory))
